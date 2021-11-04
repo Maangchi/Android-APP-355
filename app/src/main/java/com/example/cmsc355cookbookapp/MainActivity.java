538478bd4254a2451b734a3f50fee9ab5e6f863f
Maangchi <willoughbybm@vcu.edu>
2021-11-04 09:52:43 -0400

    Changed Ingredient button to open the ingredients page

--- app/src/main/java/com/example/cmsc355cookbookapp/MainActivity.java
+++ app/src/main/java/com/example/cmsc355cookbookapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cmsc355cookbookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,25 +11,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private
+    Button ing_btn = findViewById(R.id.ing_btn);
+    Button shopping_list = findViewById(R.id.Shopping_btn);
+    Button ing_btn_press = findViewById(R.id.ing_btn);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView ing_btn = findViewById(R.id.textView2);
-        Button shopping_list = findViewById(R.id.Shopping_btn);
-        Button ing_btn_press = findViewById(R.id.ing_btn);
         ing_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Log.i("MyApp","This is the ingredients");
-                Toast.makeText(getApplicationContext(), "Open the Pantry", Toast.LENGTH_SHORT)
-                        .show();
+            public void onClick(View v) {
+                openingredients_page();
             }
         });
     }
-    public void onIngbtnClick (View view) {
-        setContentView(R.layout.ingredients_page);
+
+    public void openingredients_page() {
+        Intent intent = new Intent(this, ingredientsPage.class);
+        startActivity(intent);
     }
 }
