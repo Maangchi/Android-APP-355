e811e56af13da0649f354106afa566d1f2da1585
Maangchi <willoughbybm@vcu.edu>
2021-12-10 19:21:38 -0500

    added some colorful buttons and recipe database

--- app/src/main/java/com/example/cmsc355cookbookapp/MainActivity.java
+++ app/src/main/java/com/example/cmsc355cookbookapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -45,16 +46,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openPantry() {
+        Toast.makeText(MainActivity.this, "Opening Pantry!", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, Pantry.class);
         startActivity(intent);
     }
 
     public void openRecipes() {
+        Toast.makeText(MainActivity.this, "Opening Recipe Book!", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, recipes.class);
         startActivity(intent);
     }
 
     public void openShoppingPage() {
+        Toast.makeText(MainActivity.this, "Shopping Time!", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, ShoppingPage.class);
         startActivity(intent);
     }
